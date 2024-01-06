cd05fb64d31cdc1ab2dcc8bc309ab0317b02bc86
Thijmen Kort <118896016+Thijmen1@users.noreply.github.com>
2024-01-06 13:52:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -31,8 +31,7 @@ stock_info = yf.Ticker(selected_stock)
 company_name = stock_info.info['longName']
 
 # Show selected company name
-st.text('Selected company:')
-st.header(f'{company_name}')
+st.subheader(f'{company_name}')
 
 # Years for backtesting
 n_years_backtest = st.slider('Years for backtesting:', 1, 4)
