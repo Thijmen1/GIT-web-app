096c69c50777aa166483f942828c7658670dadd8
Thijmen Kort <118896016+Thijmen1@users.noreply.github.com>
2024-02-11 13:38:19 +0100

    Update Signals.py

--- pages/Signals.py
+++ pages/Signals.py
@@ -162,6 +162,13 @@ if ticker_option == "Choose from list":
 else:
     ticker = st.text_input('Enter stock ticker', '').upper()
 
+# Determine the full company name
+stock_info = yf.Ticker(selected_stock)
+company_name = stock_info.info['longName']
+
+# Show selected company name
+st.subheader(f'{company_name}')
+
 # Check if a ticker is provided
 if ticker:
     try:
