7f956146d73b274e57652f4ef72db67905902eaf
Thijmen Kort <118896016+Thijmen1@users.noreply.github.com>
2024-02-11 01:49:30 +0100

    Update Sentiment.py

--- pages/Sentiment.py
+++ pages/Sentiment.py
@@ -69,40 +69,34 @@ def score_news(parsed_news_df):
 
 # Plot hourly sentiment
 def plot_hourly_sentiment(parsed_and_scored_news, ticker):
-    # Select only numeric columns for resampling
-    numeric_columns = parsed_and_scored_news.select_dtypes(include='number')
-    
-    # Calculate mean sentiment scores
-    mean_scores = numeric_columns.resample('H').mean()
-    
+    mean_scores = parsed_and_scored_news.resample('H').mean()
     fig = px.bar(mean_scores, x=mean_scores.index, y='Sentiment Score', title=ticker + ' Hourly Sentiment Scores')
     fig.update_xaxes(title="Time")
     fig.update_yaxes(title="Sentiment Score")
     
-    # Set x-axis date format
-    fig.update_layout(xaxis=dict(tickmode='linear', 
-                                  tickformat='%Y-%m-%d',
-                                  tickvals=pd.date_range(mean_scores.index.min(), mean_scores.index.max(), freq='D')))
+    # Set x-axis tick labels to date only
+    fig.update_layout(xaxis=dict(
+        tickmode='array',
+        tickvals=mean_scores.index,
+        ticktext=mean_scores.index.strftime('%Y-%m-%d %I %p')
+    ))
     
     return fig
 
 
 # Plot daily sentiment
 def plot_daily_sentiment(parsed_and_scored_news, ticker):
-    # Select only numeric columns for resampling
-    numeric_columns = parsed_and_scored_news.select_dtypes(include='number')
-    
-    # Calculate mean sentiment scores
-    mean_scores = numeric_columns.resample('D').mean()
-    
+    mean_scores = parsed_and_scored_news.resample('D').mean()
     fig = px.bar(mean_scores, x=mean_scores.index, y='Sentiment Score', title=ticker + ' Daily Sentiment Scores')
     fig.update_xaxes(title="Date")
     fig.update_yaxes(title="Sentiment Score")
     
-    # Set x-axis date format
-    fig.update_layout(xaxis=dict(tickmode='linear', 
-                                  tickformat='%Y-%m-%d',
-                                  tickvals=pd.date_range(mean_scores.index.min(), mean_scores.index.max(), freq='D')))
+    # Set x-axis tick labels to date only
+    fig.update_layout(xaxis=dict(
+        tickmode='array',
+        tickvals=mean_scores.index,
+        ticktext=mean_scores.index.strftime('%Y-%m-%d')
+    ))
     
     return fig
 
