5a7ceb8ee3cd69ac9fde53b8ae6ad83c295271df
Thijmen Kort <118896016+Thijmen1@users.noreply.github.com>
2024-02-11 14:08:14 +0100

    Update FB Prophet.py

--- pages/FB Prophet.py
+++ pages/FB Prophet.py
@@ -11,6 +11,7 @@ from plotly import graph_objs as go
 
 
 # Function to load historical stock data
+@st.cache_resource
 def load_data(ticker, start_date, end_date):
     data = yf.download(ticker, start_date, end_date)
     data.reset_index(inplace=True)
@@ -69,29 +70,26 @@ def plot_future():
     st.plotly_chart(fig_future, use_container_width=True)
 
 
+with st.sidebar.expander("ℹ️ Information", expanded=False):
+    st.write("This page uses Facebook Prophet, which is a forecasting tool developed by Facebook's Core Data Science team.")
+    st.write("Prophet is designed for forecasting time series data, and it's particularly useful for predicting future trends in data that exhibit patterns such as seasonality and holidays.")
+
 # Get today's date
 TODAY = date.today()
 
 # Set up Streamlit app title
-st.title('FB Prophet Forecasting')
+st.header('FB Prophet Forecasting')
 
-# List of popular stock tickers to choose from
+# List of popular stock tickers
 stocks = ('AAPL', 'AMZN', 'BABA', 'GOOGL', 'JNJ', 'JPM', 'META', 'MSFT', 'V')
 
 # User input for selecting a stock either from the list or entering a custom ticker
-user_input = st.radio("Select data source:", ("Choose from list", "Enter ticker"))
-if user_input == "Choose from list":
-    selected_stock = st.selectbox('Select dataset for prediction', stocks)
+ticker_option = st.radio("Select ticker", ("Choose from list", "Enter custom ticker"))
+
+if ticker_option == "Choose from list":
+    ticker = st.selectbox('Select stock ticker', stocks)
 else:
-    custom_ticker = st.text_input("Enter ticker:")
-    selected_stock = custom_ticker.upper()
-
-# Determine the full company name
-stock_info = yf.Ticker(selected_stock)
-company_name = stock_info.info['longName']
-
-# Show selected company name
-st.subheader(f'{company_name}')
+    ticker = st.text_input('Enter stock ticker', '').upper()
 
 # Years for backtesting
 n_years_backtest = st.slider('Years for backtesting:', 1, 4)
@@ -101,85 +99,95 @@ period_backtest = n_years_backtest * 365
 n_years_future = st.slider('Years for future prediction:', 1, 4)
 period_future = n_years_future * 365
 
-# Fetch historical data for the selected stock
-historical_data = yf.download(selected_stock, TODAY - pd.DateOffset(years=20), TODAY)
-min_start_year = historical_data.index.min().year
-max_start_year = TODAY.year
+# Check if ticker is provided
+if ticker:
+    try:
+        # Fetch historical data for the selected stock
+        historical_data = yf.download(ticker, TODAY - pd.DateOffset(years=11), TODAY)
+        min_start_year = historical_data.index.min().year
+        max_start_year = TODAY.year
 
-# Determine the first possible year with data available on January 1st
-first_year_with_data = historical_data[historical_data.index.month == 1].index.min().year
+        # Determine the first possible year with data available on January 1st
+        first_year_with_data = historical_data[historical_data.index.month == 1].index.min().year
 
-# Set default start year to be 5 years ago if possible, otherwise use the first possible year
-default_start_year = max(TODAY.year - 5, first_year_with_data)
+        # Set default start year to be 5 years ago if possible, otherwise use the first possible year
+        default_start_year = max(TODAY.year - 5, first_year_with_data)
 
-# Start date selection
-start_year = st.slider('Select start year:', first_year_with_data, max_start_year, default_start_year)
-START = f'{start_year}-01-01'
+        # Start date selection
+        start_year = st.slider('Select start year:', first_year_with_data, max_start_year, default_start_year)
+        START = f'{start_year}-01-01'
 
-# Display a loading message while caching historical stock data
-data_load_state = st.text('Loading data...')
-data = load_data(selected_stock, START, TODAY.strftime("%Y-%m-%d"))
-data_load_state.text('Loading data... done!')
+        # Check if there are enough years for backtesting
+        if TODAY.year - start_year < n_years_backtest:
+            st.warning(f"Please select an earlier start year or reduce the number of years for backtesting to {TODAY.year - start_year} year(s).")
+        else:
+            # Display a loading message while caching historical stock data
+            data = load_data(ticker, START, TODAY.strftime("%Y-%m-%d"))
 
-# Plot raw data
-plot_raw_data()
+            # Plot raw data
+            plot_raw_data()
 
-# Split data into training and testing sets
-test_data_end_date = TODAY
-test_data_start_date = TODAY.replace(year=TODAY.year - n_years_backtest)
-train_data = data[data['Date'] <= test_data_start_date.strftime("%Y-%m-%d")]
-test_data = data[(data['Date'] > test_data_start_date.strftime("%Y-%m-%d")) &
-                 (data['Date'] <= test_data_end_date.strftime("%Y-%m-%d"))]
+            # Split data into training and testing sets
+            test_data_end_date = TODAY
+            test_data_start_date = TODAY.replace(year=TODAY.year - n_years_backtest)
+            train_data = data[data['Date'] <= test_data_start_date.strftime("%Y-%m-%d")]
+            test_data = data[(data['Date'] > test_data_start_date.strftime("%Y-%m-%d")) &
+                             (data['Date'] <= test_data_end_date.strftime("%Y-%m-%d"))]
 
-# Backtesting with Prophet
-df_train_backtest = train_data[['Date', 'Close']]
-df_train_backtest = df_train_backtest.rename(columns={"Date": "ds", "Close": "y"})
+            # Backtesting with Prophet
+            df_train_backtest = train_data[['Date', 'Close']]
+            df_train_backtest = df_train_backtest.rename(columns={"Date": "ds", "Close": "y"})
 
-m_backtest = Prophet()
-m_backtest.fit(df_train_backtest)
-future_backtest = m_backtest.make_future_dataframe(periods=period_backtest)
-forecast_backtest = m_backtest.predict(future_backtest)
+            m_backtest = Prophet()
+            m_backtest.fit(df_train_backtest)
+            future_backtest = m_backtest.make_future_dataframe(periods=period_backtest)
+            forecast_backtest = m_backtest.predict(future_backtest)
 
-# Backtest header
-st.subheader('**Backtest**')
+            # Backtest header
+            st.subheader('**Backtest**')
 
-# Plot backtest forecast
-plot_backtest()
+            # Plot backtest forecast
+            plot_backtest()
 
-# Backtest components
-backtest_expander = st.expander("**Backtest components**", expanded=False)
+            # Backtest components
+            backtest_expander = st.expander("**Backtest components**", expanded=False)
 
-with backtest_expander:
-    # Plot components of the backtest forecast
-    backtest_components = m_backtest.plot_components(forecast_backtest)
-    st.write(backtest_components)
+            with backtest_expander:
+                # Plot components of the backtest forecast
+                backtest_components = m_backtest.plot_components(forecast_backtest)
+                st.write(backtest_components)
 
-# Compare backtest forecast with actual values
-compare_df = pd.merge(test_data[['Date', 'Close']], forecast_backtest[['ds', 'yhat']], how='inner', left_on='Date',
-                      right_on='ds')
+            # Compare backtest forecast with actual values
+            compare_df = pd.merge(test_data[['Date', 'Close']], forecast_backtest[['ds', 'yhat']], how='inner', left_on='Date',
+                                  right_on='ds')
 
-# Plot backtest comparison
-plot_backtest_comparison()
+            # Plot backtest comparison
+            plot_backtest_comparison()
 
-# Future prediction with Prophet
-df_train_future = data[['Date', 'Close']]
-df_train_future = df_train_future.rename(columns={"Date": "ds", "Close": "y"})
+            # Future prediction with Prophet
+            df_train_future = data[['Date', 'Close']]
+            df_train_future = df_train_future.rename(columns={"Date": "ds", "Close": "y"})
 
-m_future = Prophet()
-m_future.fit(df_train_future)
-future_future = m_future.make_future_dataframe(periods=period_future)
-forecast_future = m_future.predict(future_future)
+            m_future = Prophet()
+            m_future.fit(df_train_future)
+            future_future = m_future.make_future_dataframe(periods=period_future)
+            forecast_future = m_future.predict(future_future)
 
-# Future header
-st.subheader('**Future**')
+            # Future header
+            st.subheader('**Future**')
 
-# Plot future forecast
-plot_future()
+            # Plot future forecast
+            plot_future()
 
-# Forecast components
-forecast_expander = st.expander("**Forecast components**", expanded=False)
+            # Forecast components
+            forecast_expander = st.expander("**Forecast components**", expanded=False)
 
-with forecast_expander:
-    # Plot components of the future forecast
-    forecast_components = m_future.plot_components(forecast_future)
-    st.write(forecast_components)
+            with forecast_expander:
+                # Plot components of the future forecast
+                forecast_components = m_future.plot_components(forecast_future)
+                st.write(forecast_components)
+
+    except Exception as e:
+        st.warning("Enter a correct stock ticker, e.g. 'AAPL' above and hit Enter.")
+else:
+    st.warning("Enter a stock ticker to start forecasting.")
