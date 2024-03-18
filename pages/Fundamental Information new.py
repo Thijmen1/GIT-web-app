3869b3d8c922b4e4bb03114622fc8f770fa39b55
cevertz <155637038+cevertz@users.noreply.github.com>
2024-03-18 11:38:11 +0100

    Update Fundamental Information new.py

--- pages/Fundamental Information new.py
+++ pages/Fundamental Information new.py
@@ -90,7 +90,7 @@ def fetch_opinions(current_ticker):
 
 def main():
     st.title("Stock Analysis")
-    api_key = 'YOUR_ALPHA_VANTAGE_API_KEY'  # Replace with your Alpha Vantage API key
+    api_key = 'YOUR_API_KEY'  # Replace with your Alpha Vantage API key
     
     ticker = st.text_input('Enter stock ticker').upper()  # Update with more tickers if needed
     
